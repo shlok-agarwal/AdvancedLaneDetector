faaf7f4359382d8590e0a3787e77b60ec6376a50
shlok <ssagarwal@wpi.edu>
2021-01-18 00:13:39 -0500

    sobel operator implemented

--- helper.py
+++ helper.py
@@ -140,4 +140,93 @@ def get_cal_mtx(test_img, nx = 8, ny = 6, fname_dir = 'camera_cal/calibration*.j
 def cal_undistort(img, mtx, dist): 
 	return cv2.undistort(img, mtx, dist, None, mtx)
 
+def applySobelOperator(image, plotVisual = False):
 
+	def abs_sobel_thresh(img, orient='x', sobel_kernel=3, thresh=(20, 100)):
+		
+		# Apply the following steps to img
+		# 1) Convert to grayscale
+		gray = cv2.cvtColor(img, cv2.COLOR_RGB2GRAY)
+
+		# 2) Take the derivative in x or y given orient = 'x' or 'y'
+		if orient == 'x':
+			sobel = cv2.Sobel(gray, cv2.CV_64F, 1, 0, ksize=sobel_kernel)
+		else: 
+			sobel = cv2.Sobel(gray, cv2.CV_64F, 0, 1, ksize=sobel_kernel)
+		# 3) Take the absolute value of the derivative or gradient
+		abs_sobel = np.absolute(sobel)
+
+		# 4) Scale to 8-bit (0 - 255) then convert to type = np.uint8
+		scaled_sobel = np.uint8(255*abs_sobel/np.max(abs_sobel))
+		# 5) Create a mask of 1's where the scaled gradient magnitude 
+				# is > thresh_min and < thresh_max
+		sbinary = np.zeros_like(scaled_sobel)
+		sbinary[(scaled_sobel >= thresh[0]) & (scaled_sobel <= thresh[1])] = 1
+		# 6) Return this mask as your binary_output image
+		return sbinary
+
+	def mag_thresh(img, sobel_kernel=3, mag_thresh=(0, 255)):    
+		# Apply the following steps to img
+		# 1) Convert to grayscale
+		gray = cv2.cvtColor(img, cv2.COLOR_RGB2GRAY)
+		# 2) Take the gradient in x and y separately
+		sobelx = cv2.Sobel(gray, cv2.CV_64F, 1, 0, ksize=sobel_kernel)
+		sobely = cv2.Sobel(gray, cv2.CV_64F, 0, 1, ksize=sobel_kernel)
+		# 3) Calculate the magnitude 
+		abs_sobelxy = np.sqrt(sobelx**2 + sobely**2)
+		# 4) Scale to 8-bit (0 - 255) and convert to type = np.uint8
+		scaled_sobel = np.uint8(255*abs_sobelxy/np.max(abs_sobelxy))
+		# 5) Create a binary mask where mag thresholds are met
+		sbinary = np.zeros_like(scaled_sobel)
+		sbinary[(scaled_sobel >= mag_thresh[0]) & (scaled_sobel <= mag_thresh[1])] = 1
+		# 6) Return this mask as your binary_output image
+		return sbinary
+
+	def dir_threshold(img, sobel_kernel=3, thresh=(0, np.pi/2)):
+		
+		# Apply the following steps to img
+		# 1) Convert to grayscale
+		gray = cv2.cvtColor(img, cv2.COLOR_RGB2GRAY)
+		# 2) Take the gradient in x and y separately
+		sobelx = cv2.Sobel(gray, cv2.CV_64F, 1, 0, ksize=sobel_kernel)
+		sobely = cv2.Sobel(gray, cv2.CV_64F, 0, 1, ksize=sobel_kernel)
+		# 3) Take the absolute value of the x and y gradients
+		abs_sobelx = np.absolute(sobelx)
+		abs_sobely = np.absolute(sobely)
+		# 4) Use np.arctan2(abs_sobely, abs_sobelx) to calculate the direction of the gradient 
+		orientation = np.arctan2(abs_sobely, abs_sobelx)
+		# 5) Create a binary mask where direction thresholds are met
+		sbinary = np.zeros_like(orientation)
+		sbinary[(orientation >= thresh[0]) & (orientation <= thresh[1])] = 1
+		# 6) Return this mask as your binary_output image
+		return sbinary
+	
+	# Choose a Sobel kernel size
+	ksize = 3 # Choose a larger odd number to smooth gradient measurements
+
+	# Apply each of the thresholding functions
+	gradx = abs_sobel_thresh(image, orient='x', sobel_kernel=ksize, thresh=(20, 100))
+	grady = abs_sobel_thresh(image, orient='y', sobel_kernel=ksize, thresh=(20, 100))
+	mag_binary = mag_thresh(image, sobel_kernel=ksize, mag_thresh=(30, 100))
+	dir_binary = dir_threshold(image, sobel_kernel=15, thresh=(0.7, 1.3))
+	combined = np.zeros_like(dir_binary)
+	combined[((gradx == 1) & (grady == 1)) | ((mag_binary == 1) & (dir_binary == 1))] = 1
+
+	if plotVisual:
+		# Visualize undistortion
+		fig, ax = plt.subplots(2, 3, figsize=(20,10))
+		ax[0,0].imshow(image)
+		ax[0,0].set_title('Original Image', fontsize=20)
+		ax[0,1].imshow(gradx, cmap='gray')
+		ax[0,1].set_title('Sobel Grad X', fontsize=20)
+		ax[0,2].imshow(grady, cmap='gray')
+		ax[0,2].set_title('Sobel Grad Y', fontsize=20)
+		ax[1,0].imshow(mag_binary, cmap='gray')
+		ax[1,0].set_title('Sobel Mag', fontsize=20)
+		ax[1,1].imshow(dir_binary, cmap='gray')
+		ax[1,1].set_title('Sobel Dir', fontsize=20)
+		ax[1,2].imshow(combined, cmap='gray')
+		ax[1,2].set_title('Combined', fontsize=20)
+		plt.show()
+	
+	return combined
--- main.py
+++ main.py
@@ -4,24 +4,34 @@ import matplotlib.image as mpimg
 import numpy as np
 import cv2
 
+showDebugPlots = True
+
 #### CALIBRATION ####
 
 #reading in test image
-test_img = mpimg.imread('camera_cal/calibration3.jpg')
+test_calib_img = mpimg.imread('camera_cal/calibration3.jpg')
 
 # get camera matrix
-mtx, dist = get_cal_mtx(test_img, 9, 6)
+mtx, dist = get_cal_mtx(test_calib_img, 9, 6)
 
 # test on image
-dst = cal_undistort(test_img, mtx, dist)
+dst = cal_undistort(test_calib_img, mtx, dist)
 
-# visualize
-# Visualize undistortion
-f, (ax1, ax2) = plt.subplots(1, 2, figsize=(20,10))
-ax1.imshow(test_img)
-ax1.set_title('Original Image', fontsize=30)
-ax2.imshow(dst)
-ax2.set_title('Undistorted Image', fontsize=30)
-plt.show()
+if showDebugPlots:
+    # Visualize undistortion
+    f, (ax1, ax2) = plt.subplots(1, 2, figsize=(20,10))
+    ax1.imshow(test_calib_img)
+    ax1.set_title('Original Image', fontsize=30)
+    ax2.imshow(dst)
+    ax2.set_title('Undistorted Image', fontsize=30)
+    plt.show()
 
 ########
+
+#### Sobel Operation for edge detection ####
+
+image = mpimg.imread('test_images/straight_lines1.jpg')
+combined = applySobelOperator(image, showDebugPlots)
+
+
+
